79c3dc0765dba036b965ac50fcebe4d98f7ce73a
huafeihuao0 <yx16395175@163.com>
2018-03-04 11:49:19 +0800

    【ch16】 重构json处理demo

--- ch16/16.4/ObjC/MyNotes/MyNotes/MasterViewController.m
+++ ch16/16.4/ObjC/MyNotes/MyNotes/MasterViewController.m
@@ -1,17 +1,3 @@
-//
-//  MasterViewController.m
-//  MyNotes
-//
-//  Created by 关东升 on 15/12/31.
-//  本书网站：http://www.51work6.com
-//  智捷课堂在线课堂：http://www.zhijieketang.com/
-//  智捷课堂微信公共号：zhijieketang
-//  作者微博：@tony_关东升
-//  作者微信：tony关东升
-//  QQ：569418560 邮箱：eorient@sina.com
-//  QQ交流群：162030268
-//
-
 #import "MasterViewController.h"
 #import "DetailViewController.h"
 
@@ -24,13 +10,19 @@
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
 
     self.detailViewController = (DetailViewController *) [[self.splitViewController.viewControllers lastObject] topViewController];
-
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"Notes" ofType:@"json"];
+    
+    //获取数据的data对象
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"Notes"
+                                                     ofType:@"json"];
     NSData *jsonData = [[NSData alloc] initWithContentsOfFile:path];
 
+    //初始化NSjson的序列化工具
     NSError *error;
-    id jsonObj = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:&error];
-    if (!jsonObj || error) {
+    id jsonObj = [NSJSONSerialization JSONObjectWithData:jsonData //数据data
+                                                 options:NSJSONReadingMutableContainers //解析成可变的数据容器
+                                                   error:&error];
+    if (!jsonObj || error)
+    {
         NSLog(@"JSON解码失败");
     }
 
@@ -45,7 +37,8 @@
 
 #pragma mark - Segues
 
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
     if ([[segue identifier] isEqualToString:@"showDetail"]) {
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
         NSMutableDictionary *dict = self.listData[indexPath.row];
