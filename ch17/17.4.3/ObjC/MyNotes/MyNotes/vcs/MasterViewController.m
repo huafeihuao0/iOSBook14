f0cd7c1017ecb9d16919fc15bc4d15263fef0913
huafeihuao0 <yx16395175@163.com>
2018-03-06 20:31:19 +0800

    【ch17】 重构AFN框架的Get型请求demo

--- ch17/17.4.3/ObjC/MyNotes/MyNotes/vcs/MasterViewController.m
+++ ch17/17.4.3/ObjC/MyNotes/MyNotes/vcs/MasterViewController.m
@@ -1,17 +1,3 @@
-//
-//  MasterViewController.m
-//  MyNotes
-//
-//  Created by 关东升 on 15/12/31.
-//  本书网站：http://www.51work6.com
-//  智捷课堂在线课堂：http://www.zhijieketang.com/
-//  智捷课堂微信公共号：zhijieketang
-//  作者微博：@tony_关东升
-//  作者微信：tony关东升
-//  QQ：569418560 邮箱：eorient@sina.com
-//  QQ交流群：162030268
-//
-
 #import "MasterViewController.h"
 #import "DetailViewController.h"
 
@@ -24,8 +10,8 @@
     [super viewDidLoad];
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
     self.detailViewController = (DetailViewController *) [[self.splitViewController.viewControllers lastObject] topViewController];
-
-    [self startRequest];
+    
+    [self afnGet];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,32 +19,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark - 开始请求Web Service
-
-- (void)startRequest {
-
-    NSString *strURL = [[NSString alloc] initWithFormat:@"http://www.51work6.com/service/mynotes/WebService.php?email=%@&type=%@&action=%@", @"<你的51work6.com用户邮箱>", @"JSON", @"query"];
-
-    strURL = [strURL stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
-
-    NSURL *url = [NSURL URLWithString:strURL];
-    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
-
-    NSURLSessionConfiguration *defaultConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
-    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:defaultConfig];
-
-    NSURLSessionDataTask *task = [manager dataTaskWithRequest:request completionHandler:^(NSURLResponse *response, id responseObject, NSError *error) {
-        NSLog(@"请求完成...");
-        if (!error) {
-            [self reloadView:responseObject];
-        } else {
-            NSLog(@"error : %@", error.localizedDescription);
-        }
-    }];
-
-    [task resume];
-
-}
 
 #pragma mark - Segues
 
@@ -107,6 +67,58 @@
         //显示
         [self presentViewController:alertController animated:true completion:nil];
     }
+}
+
+#pragma mark -【DAO】
+/***
+ * AFN发送get型请求
+ ****/
+- (void)afnGet
+{
+    //创建get请求对象
+    NSURLRequest * getRequ = [self makeGetRequ];
+    //使用默认会话配置构建afn会话管理器
+    NSURLSessionConfiguration *defSessConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager *afnManager = [[AFURLSessionManager alloc] initWithSessionConfiguration:defSessConfig];
+    
+    //get完成后回调
+    id onGetFinished=^(NSURLResponse *response,//响应对象
+                       id responseObject,//数据
+                       NSError *error)
+    {
+        NSLog(@"请求完成...");
+        if (!error)
+        {
+            [self reloadView:responseObject];
+        } else
+        {
+            NSLog(@"error : %@", error.localizedDescription);
+        }
+    };
+    
+    //创建get请求任务
+    NSURLSessionDataTask *task = [afnManager dataTaskWithRequest:getRequ
+                                               completionHandler:onGetFinished];
+    
+    //恢复get任务
+    [task resume];
+    
+}
+
+/***
+ * 创建get请求对象
+ ****/
+- (NSURLRequest *)makeGetRequ
+{
+    NSString *urlPathStr=@"http://www.51work6.com/service/mynotes/WebService.php?email=%@&type=%@&action=%@";
+    NSString *strURL = [[NSString alloc] initWithFormat:urlPathStr,
+                        @"<你的51work6.com用户邮箱>", @"JSON", @"query"];
+    
+    strURL = [strURL stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+    
+    NSURL *url = [NSURL URLWithString:strURL];
+    NSURLRequest *getRequ = [[NSURLRequest alloc] initWithURL:url];
+    return getRequ;
 }
 
 
