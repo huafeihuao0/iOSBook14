0ecb58650c35e5d1d8776e25be56f087778f5760
huafeihuao0 <yx16395175@163.com>
2018-03-07 20:17:27 +0800

    【ch18】 重构18.2.4让系统自动控制定位demo

--- ch18/18.2.4/ObjC/MyLocation/MyLocation/vcs/ViewController.m
+++ ch18/18.2.4/ObjC/MyLocation/MyLocation/vcs/ViewController.m
@@ -1,17 +1,3 @@
-//
-//  ViewController.m
-//  MyLocation
-//
-//  Created by 关东升 on 16/1/14.
-//  本书网站：http://www.51work6.com
-//  智捷课堂在线课堂：http://www.zhijieketang.com/
-//  智捷课堂微信公共号：zhijieketang
-//  作者微博：@tony_关东升
-//  作者微信：tony关东升
-//  QQ：569418560 邮箱：eorient@sina.com
-//  QQ交流群：162030268
-//
-
 #import "ViewController.h"
 #import <CoreLocation/CoreLocation.h>
 
@@ -44,7 +30,7 @@
     
     //设置自动暂停位置服务
     self.locationManager.pausesLocationUpdatesAutomatically = TRUE;
-    self.locationManager.activityType = CLActivityTypeAutomotiveNavigation;
+    self.locationManager.activityType = CLActivityTypeAutomotiveNavigation;//自动巡航
     
     //开始定位
     [self.locationManager startUpdatingLocation];
