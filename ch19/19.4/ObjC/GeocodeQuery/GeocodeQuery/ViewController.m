804770d5c9b84dde62a5348ed733a8ae4b457d52
huafeihuao0 <yx16395175@163.com>
2018-03-08 20:41:30 +0800

    【ch19】 重构19.4打开ios自带地图demo

--- ch19/19.4/ObjC/GeocodeQuery/GeocodeQuery/ViewController.m
+++ ch19/19.4/ObjC/GeocodeQuery/GeocodeQuery/ViewController.m
@@ -1,17 +1,3 @@
-//
-//  ViewController.m
-//  GeocodeQuery
-//
-//  Created by 关东升 on 16/1/15.
-//  本书网站：http://www.51work6.com
-//  智捷课堂在线课堂：http://www.zhijieketang.com/
-//  智捷课堂微信公共号：zhijieketang
-//  作者微博：@tony_关东升
-//  作者微信：tony关东升
-//  QQ：569418560 邮箱：eorient@sina.com
-//  QQ交流群：162030268
-//
-
 #import "ViewController.h"
 #import <CoreLocation/CoreLocation.h>
 #import <MapKit/MapKit.h>
@@ -25,41 +11,41 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    self.txtView.text = @"";
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (IBAction)geocodeQuery:(id)sender {
+- (IBAction)geocodeQuery:(id)sender
+{
     
     if (self.txtQueryKey.text == nil) {
         return;
     }
     
+    //本地搜索请求
     MKLocalSearchRequest *request = [[MKLocalSearchRequest alloc] init];
-    request.naturalLanguageQuery = self.txtQueryKey.text;
+//    request.naturalLanguageQuery = self.txtQueryKey.text;//搜索关键字
+    request.naturalLanguageQuery = @"北京";//搜索关键字
+    //本地搜索器
     MKLocalSearch *search = [[MKLocalSearch alloc] initWithRequest:request];
     
-    [search startWithCompletionHandler:^(MKLocalSearchResponse *response, NSError *error) {
-
-        if ([response.mapItems count] > 0) {
+    //搜索完成后回调
+    id onSearchFinished=^(MKLocalSearchResponse *response, NSError *error)
+    {
+        if ([response.mapItems count] > 0) //搜索到的地标点
+        {
             //取出最后一个地标点
             MKMapItem *lastMapItem = response.mapItems.lastObject;
+            //在ios地图中打开
             [lastMapItem openInMapsWithLaunchOptions:nil];
             //地图上设置行车路线
-//            NSDictionary* options = @{MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving};
-//            [lastMapItem openInMapsWithLaunchOptions:options];
+            //            NSDictionary* options = @{MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving};
+            //            [lastMapItem openInMapsWithLaunchOptions:options];
             
-             //多个点标注线
-//            [MKMapItem openMapsWithItems:response.mapItems launchOptions:nil];
+            //多个点标注线
+            //            [MKMapItem openMapsWithItems:response.mapItems launchOptions:nil];
         }
         
-    }];
+    };
+    
+    //开始搜索
+    [search startWithCompletionHandler:onSearchFinished];
 
     //关闭键盘
     [self.txtQueryKey resignFirstResponder];
